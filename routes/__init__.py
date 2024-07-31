3c5be77d402d1f6088989f6e7eda1634486b523f
srijanid <srijanidas06@gmail.com>
2024-07-31 11:49:55 +0530

    blueprint added

--- routes/__init__.py
+++ routes/__init__.py
@@ -3,5 +3,13 @@ from flask import Blueprint
 auth_bp = Blueprint('auth_bp', __name__)
 user_bp = Blueprint('user_bp', __name__)
 test_bp = Blueprint('test_bp',__name__)
+transactions_bp = Blueprint('transactions_bp',__name__)
+bill_bp = Blueprint('bill_bp',__name__)
+reminders_bp = Blueprint('reminders_bp',__name__)
+goals_bp = Blueprint('goals_bp',__name__)
+budget_bp = Blueprint('budget_bp',__name__)
+notifications_bp = Blueprint('notifications_bp',__name__)
+receipts_bp = Blueprint('receipts_bp',__name__)
+ml_bp = Blueprint('ml_bp',__name__)
 
-from . import auth, user,test
+from . import auth, user,test,transactions,bill,reminders,goals,budget,notifications,receipts,ml
