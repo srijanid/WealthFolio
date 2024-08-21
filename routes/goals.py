60d3e944ba2795522f5cc004c5bccd7952eb38a5
srijanid <srijanidas06@gmail.com>
2024-08-21 11:19:01 +0530

    goal removed comments

--- routes/goals.py
+++ routes/goals.py
@@ -1,35 +1,3 @@
-# from flask import Blueprint, request, jsonify
-# from models import Goal
-# from models import db
-
-# goals_bp = Blueprint('goals', __name__)
-
-# @goals_bp.route('/goals', methods=['GET'])
-# def get_goals():
-#     # Retrieve all goals
-#     pass
-
-# @goals_bp.route('/goals/<int:goal_id>', methods=['GET'])
-# def get_goal(goal_id):
-#     # Retrieve specific goal
-#     pass
-
-# @goals_bp.route('/goals', methods=['POST'])
-# def create_goal():
-#     # Create new goal
-#     pass
-
-# @goals_bp.route('/goals/<int:goal_id>', methods=['PUT'])
-# def update_goal(goal_id):
-#     # Update goal
-#     pass
-
-# @goals_bp.route('/goals/<int:goal_id>', methods=['DELETE'])
-# def delete_goal(goal_id):
-#     # Delete goal
-#     pass
-
-
 from flask import Blueprint, request, jsonify
 from models import Goal, db
 
