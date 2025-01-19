26a297d2ffc4a0d69cd63ee5b228c1ae585ec820
srijanid <srijanidas06@gmail.com>
2025-01-19 18:28:29 +0530

    receipts updated

--- routes/receipts.py
+++ routes/receipts.py
@@ -1,13 +1,18 @@
 from flask import Blueprint, request, jsonify
+from flask_jwt_extended import get_jwt_identity, jwt_required
 from models import Receipt
 from models import db
 from . import auth_bp
 
 #receipts_bp = Blueprint('receipts', __name__)
 
-@auth_bp.route('/receipts', methods=['GET'])
+@auth_bp.route('/receipts/<int:user_id>', methods=['GET'])
+@jwt_required()  # Ensure the user is authenticated
 # Retrieve all receipts for a user
 def get_receipts_for_user(user_id):
+    user_id = get_jwt_identity()  # Extract the user ID from the JWT token
+    if not user_id:
+        return jsonify({"error": "User not authenticated"}), 401
     receipts = Receipt.query.filter_by(UserId=user_id).all()
     receipt_list = [{
         'ReceiptId': receipt.ReceiptId,
